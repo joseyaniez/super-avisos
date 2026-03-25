6fb61e5d0cf20cfd2f9d1e9fa580bf86c18ad197
José Yáñez <joseyanez2298@gmail.com>
2026-03-25 17:03:21 -0500

    package: instalación de tailwind

--- src/lib/components/AdminNavbar.svelte
+++ src/lib/components/AdminNavbar.svelte
@@ -3,7 +3,7 @@
 let { tab = $bindable('nuevo') } = $props();
 </script>
 
-<div>
+<div class="bg-red-300">
   <div>
     <button onclick={() => tab = 'nuevo'}>Nuevo</button>
     <button onclick={() => {
